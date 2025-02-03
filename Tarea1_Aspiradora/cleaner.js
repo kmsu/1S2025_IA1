c3896d78c3582123fc31f760de59cf610edfbdd8
kmsu <1693936230101@ingenieria.usac.edu.gt>
2025-02-03 02:13:02 -0600

    [UPDATE] tarea 1

--- Tarea1_Aspiradora/cleaner.js
+++ Tarea1_Aspiradora/cleaner.js
@@ -7,40 +7,39 @@ function reflex_agent(location, state) {
     else if (location == "B") return "LEFT";
 }
 
-function test(states, visitedStates) {
+function test(states) {
+    //ubica donde esta posicionado
     var location = states[0];
+    //condicion ? valor si verdadero : valor si falso
     var state = states[0] == "A" ? states[1] : states[2];
+    //ejecuta la funcion que evalua el estado y decide si limpia o mueve a izquierda o derecha
     var action_result = reflex_agent(location, state);
-
-    // Log the action taken
-    document.getElementById("log").innerHTML += "<br>Location: " + location + " | Action: " + action_result;
-
-    // Mark the state as CLEAN if action is CLEAN
+    //imprime la accion
+    document.getElementById("log").innerHTML += "<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
     if (action_result == "CLEAN") {
         if (location == "A") states[1] = "CLEAN";
         else if (location == "B") states[2] = "CLEAN";
     }
-    // Move to the next location based on action
-    else if (action_result == "RIGHT" && states[0] == "A") states[0] = "B";
-    else if (action_result == "LEFT" && states[0] == "B") states[0] = "A";		
-
-    // Record the visited state (a combination of location and cleanliness of both places)
-    let currentState = states[0] + states[1] + states[2];
-    if (!visitedStates.includes(currentState)) {
-        visitedStates.push(currentState); // only add unique states
+    else {
+        if (states[0] == "A" && states[2] == "CLEAN" && states[1] == "CLEAN"){
+            states[0] = "B"
+            states[1] = "DIRTY"
+            states[2] = "DIRTY"
+        }
+        else if (action_result == "RIGHT") states[0] = "B";
+        else if (action_result == "LEFT") states[0] = "A";
     }
-
-    // Check if we have visited all 8 states (A clean, B clean in all combinations)
-    if (visitedStates.length >= 8) {
-        document.getElementById("log").innerHTML += "<br>All states visited. Stopping.";
-        return;
-    }
-
-    // Continue the test after 2 seconds
-    setTimeout(function () { test(states, visitedStates); }, 2000);
+    setTimeout(function () { test(states); }, 2000);
 }
 
-// Initial states and visited states tracking
-var states = ["A", "DIRTY", "DIRTY"];
-var visitedStates = [];
-test(states, visitedStates);
+var states = ["A","DIRTY","DIRTY"];
+
+//var states = ["A","DIRTY","CLEAN"];
+//var states = ["A","CLEAN","DIRTY"];
+//var states = ["A","CLEAN","CLEAN"];
+//var states = ["B","DIRTY","DIRTY"];
+//var states = ["B","DIRTY","CLEAN"];
+//var states = ["B","CLEAN","DIRTY"];
+//var states = ["B","CLEAN","CLEAN"];
+
+test(states);
